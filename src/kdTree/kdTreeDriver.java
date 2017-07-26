bed435eb7a7a85509d751173b89472cc78855b70
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2017-07-26 10:23:22 -0400

    Adding code kdTreeDriver.java

--- src/kdTree/kdTreeDriver.java
+++ src/kdTree/kdTreeDriver.java
@@ -1,7 +1,19 @@
 package kdTree;
 
+import javafx.geometry.Point2D;
+
 public class kdTreeDriver {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        kdTree kd = new kdTree();
+        kd.insert(new Point2D(3, 6));
+        kd.insert(new Point2D(17, 15));
+        kd.insert(new Point2D(13, 15));
+        kd.insert(new Point2D(6, 12));
+        kd.insert(new Point2D(9, 1));
+        kd.insert(new Point2D(2, 7));
+        kd.insert(new Point2D(10, 19));
+
     }
 }
