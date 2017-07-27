c1f9c5f80473f03d4535826679b5557151855f32
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2017-07-27 09:35:46 -0400

    Code to perform range queries.

--- src/kdTree/kdTree.java
+++ src/kdTree/kdTree.java
@@ -1,64 +1,168 @@
 package kdTree;
 
+import javafx.geometry.BoundingBox;
 import javafx.geometry.Point2D;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class kdTree {
 
-    //k is the no of dimensions
-    final int k = 2;
+    public static final Comparator<Point2D> X_ORDER = new XOrder();
+
+    private static class XOrder implements Comparator<Point2D> {
+        public int compare(Point2D p, Point2D q) {
+            if (p.getX() < q.getX()) return -1;
+            if (p.getX() > q.getX()) return +1;
+            return 0;
+        }
+    }
+
+    public static final Comparator<Point2D> Y_ORDER = new YOrder();
+
+    private static class YOrder implements Comparator<Point2D> {
+        public int compare(Point2D p, Point2D q) {
+            if (p.getY() < q.getY()) return -1;
+            if (p.getY() > q.getY()) return +1;
+            return 0;
+        }
+    }
 
     class Node {
         private Point2D point;
         private Node left;
         private Node right;
+        private boolean vertical;
 
-        public Node() {
-
-        }
-
-        public Node(Point2D point) {
+        public Node(Point2D point, Node left, Node right, boolean vertical) {
             this.point = point;
-            this.left = null;
-            this.right = null;
-        }
 
-        public Node(Point2D point, Node left, Node right) {
-            this.point = point;
             this.left = left;
             this.right = right;
+
+            //Vertical is true when the split is along the Y axis
+            this.vertical = vertical;
+        }
+
+        public Point2D getPoint() {
+            return point;
+        }
+
+        public void setPoint(Point2D point) {
+            this.point = point;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public boolean isVertical() {
+            return vertical;
+        }
+
+        public void setVertical(boolean vertical) {
+            this.vertical = vertical;
         }
     }
 
     private Node root;
+    private int n;
 
-    private Node insertHelper(Node root, Point2D point, int depth) {
-        if (root == null) {
-            return new Node(point);
+    public kdTree() {
+        this.root = null;
+        this.n = 0;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    private Node insertHelper(Node node, Point2D point, boolean vertical) {
+        if (node == null) {
+            n = n + 1;
+            return new Node(point, null, null, vertical);
         }
 
-        int currentDimension = depth % k;
+        Point2D nodePoint = node.getPoint();
 
-        //If currentDimension is 0, x-axis, else, y-axis
-        if (currentDimension == 0) {
-            if (point.getX() < root.point.getX()) {
-                root.left = insertHelper(root.left, point, depth + 1);
-            } else {
-                root.right = insertHelper(root.right, point, depth + 1);
-            }
+        if (nodePoint.equals(point)) {
+            return node;
+        }
 
-        } else if (currentDimension == 1) {
-            if (point.getY() < root.point.getY()) {
-                root.left = insertHelper(root.left, point, depth + 1);
-            } else {
-                root.right = insertHelper(root.right, point, depth + 1);
-            }
+        Comparator<Point2D> comparator = X_ORDER;
+
+        if (vertical) {
+            comparator = Y_ORDER;
+        }
+
+        int comparatorResult = comparator.compare(point, nodePoint);
+
+        if (comparatorResult < 0) {
+            node.setLeft(insertHelper(node.getLeft(), point, !vertical));
         } else {
-            //
+            node.setRight(insertHelper(node.getRight(), point, !vertical));
         }
-        return root;
+
+        return node;
     }
 
     public void insert(Point2D point) {
-        root = insertHelper(root, point, 0);
+        root = insertHelper(root, point, true);
     }
+
+    public List<Point2D> rangeLookup(BoundingBox boundingBox) {
+        List<Point2D> selected = new ArrayList<Point2D>();
+        lookupHelper(root, boundingBox, selected);
+        return selected;
+    }
+
+    private void lookupHelper(Node node, BoundingBox boundingBox, List<Point2D> selected) {
+
+        if (node == null) {
+            return;
+        }
+
+        Point2D currPoint = node.getPoint();
+
+        if (boundingBox.contains(currPoint)) {
+            selected.add(currPoint);
+        }
+
+        double pointCoordinate = node.getPoint().getY();
+        double minCoordinate = boundingBox.getMinY();
+        double maxCoordinate = boundingBox.getMaxY();
+
+        if (node.isVertical()) {
+            pointCoordinate = node.getPoint().getX();
+            minCoordinate = boundingBox.getMinX();
+            maxCoordinate = boundingBox.getMaxY();
+        }
+
+        if (minCoordinate < pointCoordinate) {
+            lookupHelper(node.getLeft(), boundingBox, selected);
+        }
+
+        if (maxCoordinate >= pointCoordinate) {
+            lookupHelper(node.getRight(), boundingBox, selected);
+        }
+
+    }
+
 }
--- src/kdTree/kdTreeDriver.java
+++ src/kdTree/kdTreeDriver.java
@@ -1,12 +1,16 @@
 package kdTree;
 
+import javafx.geometry.BoundingBox;
 import javafx.geometry.Point2D;
+
+import java.util.List;
 
 public class kdTreeDriver {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
         kdTree kd = new kdTree();
+
         kd.insert(new Point2D(3, 6));
         kd.insert(new Point2D(17, 15));
         kd.insert(new Point2D(13, 15));
@@ -15,5 +19,12 @@ public class kdTreeDriver {
         kd.insert(new Point2D(2, 7));
         kd.insert(new Point2D(10, 19));
 
+        System.out.println(kd.size());
+
+        BoundingBox bbox = new BoundingBox(1, 1, 2, 5);
+        System.out.println(bbox.getMaxX() + " " + bbox.getMaxY());
+
+        List<Point2D> found = kd.rangeLookup(new BoundingBox(1, 1, 2, 5));
+        System.out.println(found.toString());
     }
 }
